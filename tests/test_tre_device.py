8e1e4b673a70794402e88becd45a912928d1a05b
Ronnie Borchard Jr. <sanebvillain@gmail.com>
2026-04-19 17:20:11 -0700

    Create unit tests for tre_device.py

---
+++ tests/test_tre_device.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestTreDevice(unittest.TestCase):
+    def test_example(self):
+        # Add your test code here
+        self.assertTrue(True)  # Dummy assertion
+
+if __name__ == '__main__':
+    unittest.main()
